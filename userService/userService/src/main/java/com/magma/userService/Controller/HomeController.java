b056016e8b2c8081864712e227440e38deba471c
mohit <mohitrakheja007@gmail.com>
2025-01-31 19:43:29 +0530

    user microservice

---
+++ userService/userService/src/main/java/com/magma/userService/Controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.magma.userService.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+}
---
+++ userService/userService/src/test/java/com/magma/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magma.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
