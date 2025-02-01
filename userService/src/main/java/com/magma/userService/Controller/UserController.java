a384b1a08a390d2c3bc01b2614ae294ecdab285a
mohit <mohitrakheja007@gmail.com>
2025-02-01 22:59:19 +0530

    find all and get by Id API

--- userService/src/main/java/com/magma/userService/Controller/UserController.java
+++ userService/src/main/java/com/magma/userService/Controller/UserController.java
@@ -3,9 +3,10 @@ package com.magma.userService.Controller;
 import com.magma.userService.Repository.UserRepository;
 import com.magma.userService.modal.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -17,4 +18,17 @@ public class UserController {
     User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
+    @GetMapping("/getAllUsers")
+    List<User>getAllUsers(){
+        return userRepository.findAll();
+    }
+    @GetMapping("/getUser/{Id}")
+    User getUserById(@PathVariable("Id") Long id){
+        Optional<User>user= userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new RuntimeException("User Not Found");
+    }
+
 }
